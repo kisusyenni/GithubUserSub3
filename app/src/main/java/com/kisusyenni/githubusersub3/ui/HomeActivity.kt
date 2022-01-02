92703a425cd681d041847c9c7798780ba42b116c
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-01-02 12:30:45 +0700

    Initial commit

---
+++ app/src/main/java/com/kisusyenni/githubusersub3/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.kisusyenni.githubusersub3.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kisusyenni.githubusersub3.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

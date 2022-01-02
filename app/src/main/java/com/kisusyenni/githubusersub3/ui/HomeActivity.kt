0f55d84cac8e562423661e957012e10e2fd650df
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-01-02 12:32:45 +0700

    Rename main layout to home

--- app/src/main/java/com/kisusyenni/githubusersub3/ui/HomeActivity.kt
+++ app/src/main/java/com/kisusyenni/githubusersub3/ui/HomeActivity.kt
@@ -7,6 +7,6 @@ import com.kisusyenni.githubusersub3.R
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
